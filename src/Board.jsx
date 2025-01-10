2cc3f1fb6c65778716d321ffff66389ea0c6822d
sami855-ux <samitale56@outlook.com>
2025-01-10 21:48:04 +0500

    Board is added with each column

---
+++ src/Board.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react"
+import { Column } from "./Column"
+
+export const Board = () => {
+  const [cards, setCards] = useState([])
+
+  return (
+    <div className="flex w-full h-full gap-3 p-12 overflow-scroll">
+      <Column
+        title="Backlog"
+        column="backlog"
+        headingColor="text-neutral-500"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="TODO"
+        column="todo"
+        headingColor="text-yellow-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="In Progress"
+        column="doing"
+        headingColor="text-blue-200"
+        cards={cards}
+        setCards={setCards}
+      />
+      <Column
+        title="Complete"
+        column="done"
+        headingColor="text-emerald-200"
+        cards={cards}
+        setCards={setCards}
+      />
+    </div>
+  )
+}
