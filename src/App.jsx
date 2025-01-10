1c2bb3cb93c589aa2a0add783528e5f22447fd5a
sami855-ux <samitale56@outlook.com>
2025-01-10 21:34:19 +0500

    Blank pages are added

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return <div className="w-full h-screen">Hello</div>
+}
+
+const Board = () => {
+  return <div className="flex w-full h-full gap-3 p-12 overflow-scroll"></div>
+}
