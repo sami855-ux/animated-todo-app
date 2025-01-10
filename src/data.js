5d0a5da3fde3650a47a6c2bd65c9307356411e62
sami855-ux <samitale56@outlook.com>
2025-01-10 22:12:19 +0500

    Data for the website is added(Dummy data)

---
+++ src/data.js
@@ -0,0 +1,44 @@
+const DEFAULT_CARDS = [
+  {
+    title: "Look into render bug in dashboard",
+    id: "1",
+    column: "backlog",
+  },
+  {
+    title: "Sox complicated checklist",
+    id: "2",
+    column: "backlog",
+  },
+  {
+    title: "Document Notification service",
+    id: "3",
+    column: "backlog",
+  },
+  {
+    title: "Research DB options for new microservices",
+    id: "4",
+    column: "todo",
+  },
+  {
+    title: "Postmortem for outage",
+    id: "5",
+    column: "todo",
+  },
+  {
+    title: "Sync with product on Q3 roadmap",
+    id: "6",
+    column: "todo",
+  },
+  {
+    title: "Refactor context providers to use Zustand",
+    id: "7",
+    column: "doing",
+  },
+  {
+    title: "set up DD dashboard for lambda listeners",
+    id: "8",
+    column: "done",
+  },
+]
+
+export { DEFAULT_CARDS }
