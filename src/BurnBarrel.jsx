c4722bbdafe904db72a93dac7c7d233bed2f20f7
sami855-ux <samitale56@outlook.com>
2025-01-13 09:34:58 +0500

    Component for deleting the todo list is added

---
+++ src/BurnBarrel.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import PropTypes from "prop-types"
+
+import { FaFire } from "react-icons/fa"
+import { FiTrash } from "react-icons/fi"
+
+export const BurnBarrel = ({ setCards }) => {
+  const [active, setActive] = useState(false)
+
+  const handleDragOver = (e) => {
+    e.preventDefault()
+    setActive(true)
+  }
+
+  const handleDragLeave = () => {
+    setActive(false)
+  }
+
+  const handleDragEnd = (e) => {
+    const cardId = e.dataTransfer.getData("cardId")
+
+    setCards((prv) => prv.filter((c) => c.id !== cardId))
+
+    setActive(false)
+  }
+  return (
+    <div
+      onDrop={handleDragEnd}
+      onDragOver={handleDragOver}
+      onDragLeave={handleDragLeave}
+      className={`mt-10 grid h-56 w-56 shrink-0 place-content-center rounded border text-3xl ${
+        active
+          ? "border-red-800 bg-red-800/20 text-red-500"
+          : "border-neutral-500 bg-neutral-500/20 text-neutral-500"
+      }`}
+    >
+      {active ? (
+        <FaFire size={25} className="animate-bounce" />
+      ) : (
+        <FiTrash size={25} />
+      )}
+    </div>
+  )
+}
+
+BurnBarrel.propTypes = {
+  setCards: PropTypes.func,
+}
